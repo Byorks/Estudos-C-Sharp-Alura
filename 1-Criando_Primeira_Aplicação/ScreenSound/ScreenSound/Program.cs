dd7c7732c8ad69599b0ac4fc80b738fb1565f766
Byorks <vanessabyorksp@gmail.com>
2024-11-19 15:53:34 -0300

    ✨ feat: creating string vars

--- 1-Criando_Primeira_Aplicação/ScreenSound/ScreenSound/Program.cs
+++ 1-Criando_Primeira_Aplicação/ScreenSound/ScreenSound/Program.cs
@@ -2,6 +2,36 @@
 
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 // C# é uma linguagem fortemente tipada, exige o uso de ;(ponto e vírgula) no final das linhas.
-
 // Em C# utilizamos o padrão de camelCase para criação de variáveis, primeira minúscula e as demias maiúsculas
-Console.WriteLine(mensagemDeBoasVindas);
+
+// Função sem retorno -> void
+// Funções utilizamos o padrão de PascalCase, todo começo de palavra usamos maiúsculas
+void ExibirMensagemDeBoasVindas()
+{
+    // Verbatim para mostrar como a string vai aparecer em console 
+    Console.WriteLine(@"
+        
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+}
+
+// Executando as funções
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
